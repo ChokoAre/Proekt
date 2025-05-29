3128b8cc7de8949ede72dc81c125c3691517be02
ChokoAre <chochigeorgiev@gmail.com>
2025-05-30 00:25:53 +0300

    Add files via upload

---
+++ proekt/src/proekt/DuplicateRecipeException.java
@@ -0,0 +1,7 @@
+package proekt;
+
+public class DuplicateRecipeException extends Exception {
+    public DuplicateRecipeException(String message) {
+        super(message);
+    }
+}
---
+++ proekt/src/proekt/MyArrayList.java
@@ -0,0 +1,71 @@
+package proekt;
+
+public class MyArrayList<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public void add(E element) {
+        ensureCapacity();
+        elements[size++] = element;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (E) elements[index];
+    }
+
+    @SuppressWarnings("unchecked")
+    public E set(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        E oldValue = (E) elements[index];
+        elements[index] = element;
+        return oldValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        E removedElement = (E) elements[index];
+        
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        
+        elements[--size] = null;
+        return removedElement;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            int newCapacity = elements.length * 2;
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = newArray;
+        }
+    }
+}
